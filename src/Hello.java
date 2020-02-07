1f1791eb1591755e8a906b1dacd70cda86d41256
swj <swj@qq.com>
2020-02-07 12:48:36 +0800

    commit hellogit1

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
 
 	public static void test() {
-		System.out.println("hello world");
+		System.out.println("hello world 1");
 	}
 	public static void main(String[] args) {
 		new ChessFrame();
