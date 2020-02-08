2b10cf79e86133c47bbff59956fc9f613fc4e06a
swj <swj@qq.com>
2020-02-08 11:41:36 +0800

    edit on branch hot fix

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
 	public static void test() {
 		System.out.println("hello world 2");
 		System.out.println("hello world 1");
+		
+		System.out.println("hot fix .........");
 	}
 	public static void main(String[] args) {
 		new ChessFrame();
